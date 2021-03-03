9e78ec6d6a4245f478f97988d7a8ca33ca1484c4
OC929 <79985629+OC929@users.noreply.github.com>
2021-03-03 23:34:01 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics
+o = int(input("輸入一個數:"))
+s = int(input("輸入一個數:"))
+c = int(input("輸入一個數:"))
+a = int(input("輸入一個數:"))
+r = int(input("輸入一個數:"))
+w = [o,s,c,a,r]
+print("平均數:",myStatistics.myMean(w))
